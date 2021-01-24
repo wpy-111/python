8e6ba09734db54561107949cee102cbcbde34b72
wpy <1324749417@qq.com>
2021-01-24 19:03:43 +0800

    data

--- MachineLearning/day03/demo04_lr.py
+++ MachineLearning/day03/demo04_lr.py
@@ -1,8 +1,9 @@
 """
-    人工分类
+    逻辑回归模型
 """
 import numpy as np
 import matplotlib.pyplot as mp
+import sklearn.linear_model as sl
 x = np.array([
     [3, 1],
     [2, 5],
@@ -13,13 +14,14 @@ x = np.array([
     [4, 7],
     [4, -1]])
 y = np.array([0, 1, 1, 0, 0, 1, 1, 0])
+model = sl.LogisticRegression(C=1)
+model.fit(x,y)
 #生成网格坐标点
 n = 500
 l,r = x[:,0].min()- 1,x[:,0].max()+1
 b,t = x[:,1].min()-1,x[:,1].max()+1
 grid_x,grid_y = np.meshgrid(np.linspace(l,r,n),np.linspace(b,t,n))
-grid_z = np.zeros_like(grid_x)
-grid_z[grid_x<grid_y] = 1
+grid_z = model.predict(np.column_stack((grid_x.ravel(),grid_y.ravel()))).reshape(500,500)
 #绘制样点的散点图
 mp.figure('Clasification',facecolor='lightgrey')
 mp.title('Clasification',fontsize=16)
@@ -28,7 +30,7 @@ mp.scatter(x[:,0],x[:,1],c=y,cmap='brg_r',s=80,label='Samples')
 mp.legend()
 mp.tight_layout()
 mp.show()
-# ax3d = mp.gcf(projection='3d')
+
 
 
 
